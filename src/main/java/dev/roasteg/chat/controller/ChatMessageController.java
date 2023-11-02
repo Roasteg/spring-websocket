44df677f3508e5bbe85baf9434f502fb23c94bec
Roasteg <krostislav468@gmail.com>
2023-11-02 11:37:04 +0200

    Separate controllers, add get all messages controller

--- src/main/java/dev/roasteg/chat/controller/ChatMessageController.java
+++ src/main/java/dev/roasteg/chat/controller/ChatMessageController.java
@@ -1,27 +1,30 @@
 package dev.roasteg.chat.controller;
 
+import java.util.List;
+
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.roasteg.chat.dto.ChatMessageDTO;
-import dev.roasteg.chat.dto.ChatUserDTO;
-import dev.roasteg.chat.request.AddUserRequest;
 import dev.roasteg.chat.request.SendMessageRequest;
 import dev.roasteg.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatMessageController {
     
     private final ChatService chatService;
     private final SimpMessagingTemplate messagingTemplate;
-
-    @PostMapping("/add-user")
-    public ChatUserDTO addUser(@RequestBody AddUserRequest request) {
-        return chatService.addUser(request.getName());
+    
+    @GetMapping("/get-messages")
+    public List<ChatMessageDTO> getMessages() {
+        return chatService.getChatMessages();
     }
 
     @PostMapping("/send-message")
---
+++ src/main/java/dev/roasteg/chat/controller/ChatUserController.java
@@ -0,0 +1,23 @@
+package dev.roasteg.chat.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.roasteg.chat.dto.ChatUserDTO;
+import dev.roasteg.chat.request.AddUserRequest;
+import dev.roasteg.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class ChatUserController {
+    private final ChatService chatService;
+
+    @PostMapping("/add-user")
+    public ChatUserDTO addUser(@RequestBody AddUserRequest request) {
+        return chatService.addUser(request.getName());
+    }
+}
